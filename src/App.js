d7aa25a92aa85e09bf45a9a658c687c18aaa0b51
Cainan Barboza <CainanBarboza1@gmail.com>
2020-08-26 10:28:38 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import {connect} from 'react-redux'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+import React, { Component } from 'react'
+
+class App extends Component {
+  render() {
+    return (
+      <>
+        {this.props.count}
+      </>
+    )
+  }
+}// end of the class
+
+
+//purpose is to map redux global state to a prop we can use inside react
+const mapStateToProps = (state) => {
+  //mapped react prop count to state.counter
+  return{
+    count: state.counter
+  }
 }
 
-export default App;
+
+export default connect(null,null)(App)// connects App component to the provider
+
